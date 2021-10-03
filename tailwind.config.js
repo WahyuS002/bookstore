6c44b494b030dab0998c6bb54e22aaa4c33832ba
WahyuS002 <syahputrawahyu61@gmail.com>
2021-10-04 06:36:37 +0700

    :zap: installed breeze

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,33 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
     mode: "jit",
-    purge: ["./resources/**/*.blade.php"],
-    darkMode: false, // or 'media' or 'class'
+    purge: [
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+    ],
+
     theme: {
         extend: {
+            fontFamily: {
+                sans: ["Nunito", ...defaultTheme.fontFamily.sans],
+                poppins: ["poppins"],
+            },
             colors: {
                 primary: "#222439",
             },
             boxShadow: {
                 soft: "0 0 20px rgba(0, 0, 0, 0.08)",
             },
-            fontFamily: {
-                poppins: ["poppins"],
-            },
         },
     },
+
     variants: {
-        extend: {},
+        extend: {
+            opacity: ["disabled"],
+        },
     },
-    plugins: [],
+
+    plugins: [require("@tailwindcss/forms")],
 };
