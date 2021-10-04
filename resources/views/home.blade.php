000f38df97e1f56b69af82e269b9c42f451691ca
WahyuS002 <syahputrawahyu61@gmail.com>
2021-10-04 21:18:06 +0700

    :white_check_mark: Create books table and BookSeeder

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,10 +12,17 @@
 		<div class="w-[60%] my-12">
 			<div class="flex items-center justify-between">
 				<h1 class="text-primary font-bold text-2xl">Book Store</h1>
+				@guest
 				<div class="space-x-3">
-					<button class="rounded-md px-4 py-2 text-sm hover:bg-gray-100 transition duration-300 ease-in-out">Sign in</button>
-					<button class="rounded-md bg-primary hover:bg-black transition duration-300 ease-in-out text-white px-4 py-2 text-sm">Sign up</button>
+					<a href="{{ route('login') }}" class="rounded-md px-4 py-2 text-sm hover:bg-gray-100 transition duration-300 ease-in-out">Sign in</a>
+					<a href="{{ route('register') }}" class="rounded-md bg-primary hover:bg-black transition duration-300 ease-in-out text-white px-4 py-2 text-sm">Sign up</a>
 				</div>
+				@endguest
+				@auth
+				<a href="{{ route('dashboard') }}" class="rounded-md bg-primary hover:bg-black transition duration-300 ease-in-out text-white px-4 py-2 text-sm">
+					Dashboard
+				</a>
+				@endauth
 			</div>
 			<div class="space-x-4 text-sm mt-4 px-5 py-2 rounded-lg border bg-white border-gray-200">
 				<span class="font-semibold">Fixed Price</span>
