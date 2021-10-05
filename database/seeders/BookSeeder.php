21024a2f3cf32140ce14b6e7674e188fb04d3fb5
WahyuS002 <syahputrawahyu61@gmail.com>
2021-10-05 17:47:18 +0700

    :art: home

--- database/seeders/BookSeeder.php
+++ database/seeders/BookSeeder.php
@@ -18,7 +18,7 @@ class BookSeeder extends Seeder
         $now = Carbon::now();
 
         DB::table('books')->insert([
-            ['title' => 'Atomic Habit', 'category' => 'self improvement', 'payment_type' => 'closed', 'rating' => 4.6, 'price' => 75000,'cover_image' => 'cover_image/atomic-habit.jpg', 'created_at' => $now, 'updated_at' => $now],
+            ['title' => 'Atomic Habits', 'category' => 'self improvement', 'payment_type' => 'closed', 'rating' => 4.6, 'price' => 75000,'cover_image' => 'cover_image/atomic-habits.jpg', 'created_at' => $now, 'updated_at' => $now],
             ['title' => 'Harry Potter', 'category' => 'novel', 'payment_type' => 'closed', 'rating' => 4.7, 'price' => 84000,'cover_image' => 'cover_image/harry-potter.jpg', 'created_at' => $now, 'updated_at' => $now],
             ['title' => 'The Alchemist', 'category' => 'fiction', 'payment_type' => 'closed', 'rating' => 4.6, 'price' => 54000,'cover_image' => 'cover_image/the-alchemist.jpg', 'created_at' => $now, 'updated_at' => $now],
             ['title' => 'Ikigai', 'category' => 'self improvement', 'payment_type' => 'closed', 'rating' => 4.6, 'price' => 63500,'cover_image' => 'cover_image/ikigai.jpg', 'created_at' => $now, 'updated_at' => $now]
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -29,11 +29,12 @@
 				<span class="text-gray-400">Donation</span>
 			</div>
 			<div class="mt-6 grid grid-cols-4 gap-4">
-				<div class="p-3 rounded-lg border bg-white border-gray-200 hover:shadow-soft transition duration-300 ease-in-out">
-					<div class="rounded-xl bg-gray-600 h-56"></div>
+				@foreach ($books as $book)
+				<div class="p-3 rounded-lg border bg-white border-gray-200 hover:shadow-soft transition duration-300 ease-in-out cursor-pointer">
+					<img src="{{ asset('storage/' . $book->cover_image) }}" class="h-64 w-full object-cover rounded-lg" alt="">
 					<div class="mt-3">
-						<p class="font-semibold text-primary">Thunder Stunt</p>
-						<p class="text-xs text-gray-400 uppercase">Biography</p>
+						<p class="font-semibold text-primary">{{ $book->title }}</p>
+						<p class="text-xs text-gray-400 uppercase">{{ $book->category }}</p>
 					</div>
 					<div class="mt-4 flex justify-between items-center">
 						<div class="flex items-center text-yellow-600">
@@ -42,11 +43,12 @@
 									<path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
 								</svg>
 							</span>
-							<span class="ml-2 font-medium text-sm">4.6</span>
+							<span class="ml-2 font-medium text-sm">{{ $book->rating }}</span>
 						</div>
-						<p class="text-sm text-primary font-semibold">75K</p>
+						<p class="text-sm text-primary font-semibold">{{ $book->price / 1000 . 'K' }}</p>
 					</div>
 				</div>
+				@endforeach
 			</div>
 		</div>
 	</div>
